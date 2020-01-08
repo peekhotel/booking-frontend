a5947ad0c9f2e23f7aa01c6d1946075cd41054af
Zver <vladdovgal@gmail.com>
2020-01-08 22:22:29 +0200

    booking v.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatepickermoduleModule } from './datepickermodule/datepickermodule.module';
+// import {matDatepickerAnimations} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomNavComponent,
+    TopBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DatepickermoduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
