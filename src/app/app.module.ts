9076629146831ecf103523239c9aaee53100bc9d
Danny <liquidcore7@gmail.com>
2020-01-11 02:13:21 +0200

    bump angular version, remove unneeded packages & delete their usages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { DlDateTimeDateModule, DlDateTimePickerModule } from 'angular-bootstrap-datetimepicker';
 import { BottomMenuComponent } from './bottom-menu/bottom-menu.component';
 import { TopComponent } from './top-menu/top-menu.component';
 import { OrderComponent } from './order/order.component';
@@ -33,11 +31,8 @@ import { MatListModule } from '@angular/material/list';
     MainNavComponent,
   ],
   imports: [
-    NgbModule,
     BrowserModule,
     AppRoutingModule,
-    DlDateTimeDateModule,  // <--- Determines the data type of the model
-    DlDateTimePickerModule,
     FormsModule,
     BrowserAnimationsModule,
     BrowserModule,
